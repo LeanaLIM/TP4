9f2d87ab23ea5053b63758d708f8467b30cc4bad
LeanaLIM <leanalim80@gmail.com>
2023-02-15 10:08:55 +0100

    :tada: question 9
    je comprend pas.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     var date = "JJ/MM/AAAA";
+    var PositionPremierSlash = date.indexOf('/');
+    var monthPlusYear = date.substring(PositionPremierSlash + 1,10);
+    var PositionDeuxiemeSlash = monthPlusYear.indexOf('/');
 
-   function normaliseDate(){
-    date = s
+    function normaliseDate(){
+    
    }
 })
